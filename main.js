68289b8fb1d63028e2f2d692e5c4af15c98243e1
deru0u0ww <mao20.cfs@icloud.com>
2025-08-31 07:54:22 +0900

    修正

--- main.js
+++ main.js
@@ -1,78 +1,74 @@
 'use strict';
-{
-    const FizzBuzzObj = {
-        title: 'FizzBuzz',
-        createBtn: function() {
-            const main = document.querySelector('main');
-            const h1 = document.createElement('h1');
-            h1.textContent = this.title;
-            main.appendChild(h1);
 
-            const startButton = document.createElement('button');
-            startButton.id = 'start-button';
-            startButton.textContent = 'Start';
-            main.appendChild(startButton);
+    const FizzBuzz = {
+        elements:{},
+        init: function() {
 
-            const resetButton = document.createElement('button');
-            resetButton.id = 'reset-button';
-            resetButton.textContent = 'Reset';
-            main.appendChild(resetButton);
+            this.createEl();
+            this.bindBtn();
         },
-        content:'0~100までの数字を入力してFizzBuzzを出力してみよう',
-        message:'数字の入力をお願いします',
-        
+        createEl: function() {
+            const title = 'FizzBuzz';
+            const msg = '数字の入力をお願いします';
+            const alertMsg = '1~100までの数字を入力してください'
+            //タイトルボタンの生成
+            const h1El = document.querySelector('#title');
+            h1El.textContent = title;
+            //スタートボタンを生成
+            const startBtn = document.querySelector('#startBtn');
+            //リセットボタンを生成
+            const resetBtn = document.querySelector('#reset');
+            const mainEl = document.querySelector('#main');
+            const result_sectionEl = document.querySelector('#result');
+            const olEl = document.querySelector('#ol');
+            
+
+            this.elements = { msg, alertMsg,result_sectionEl,mainEl, olEl, startBtn, resetBtn };
+        },
         fizzbuzz: function() {
+            const { msg, alertMsg, mainEl, result_sectionEl, olEl} = this.elements;
             //既存の結果を削除
             const oldSection = document.getElementById('result');
             if (oldSection) oldSection.remove();
 
-            const checkMath = Number(prompt(this.message));  
+            const checkMath = Number(prompt(msg));  
             if ( isNaN(checkMath) || checkMath < 1 || checkMath > 100 ) {
-                alert('1~100までの数字を入力してください');
+                alert(alertMsg);
                 return;
             }
-
-
-            const main = document.querySelector('main');
-            const section  = document.createElement('section');
-            //結果管理のためのid付与付与
-            section.id = 'result';
-            const ol = document.createElement('ol');
 
             for (let i = 1; i <= checkMath; i ++) {
                 const li = document.createElement('li');
                 if ( i%3 === 0 && i%5 === 0 ) {
                     li.textContent = 'FizzBuzz';
-                    li.style.color = 'yellowgreen';
                 } else if( i%3 === 0 ) {
                     li.textContent = 'Fizz';
-                    li.style.color = 'blue';
                 } else if( i%5 === 0 ) {
                     li.textContent = 'Buzz';
-                    li.style.color = 'red';
                 } else {
                     li.textContent = i;
                 }
-                ol.appendChild(li);
+                olEl.appendChild(li);
             }
-                section.appendChild(ol);
-                main.appendChild(section);
+                result_sectionEl.appendChild(olEl);
+                
+                mainEl.appendChild(result_sectionEl);
         },
         reset: function() {
             const oldSection = document.getElementById('result');
             if (oldSection) oldSection.remove();
+        },
+        bindBtn: function() {
+        const { startBtn, resetBtn } = this.elements;
+        startBtn.addEventListener('click', ()=> {
+            this.fizzbuzz();
+            },false);
+        // resetBtn.addEventListener('click',()=> {
+        //     this.reset();
+        // },false);
         }
     };
-    //初期化処理
-    FizzBuzzObj.createBtn();
-    const startButton = document.getElementById('start-button');
-    startButton.addEventListener('click', ()=> {
-        FizzBuzzObj.fizzbuzz();
-        },false);
-    const resetButton = document.getElementById('reset-button');
-    resetButton.addEventListener('click',()=> {
-        FizzBuzzObj.reset();
-       
-    },false);
+    FizzBuzz.init();
+    
+    
 
-}
